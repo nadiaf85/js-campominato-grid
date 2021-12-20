6217b54dd4349a1711757556f114c01b2176078a
nadiaf85 <nadia.fiorio@alice.it>
2021-12-20 20:19:43 +0100

    easy funziona

--- myscript.js
+++ myscript.js
@@ -16,15 +16,15 @@ bottone.addEventListener('click', function(){
     let livello = document.getElementById('scelta').value;
     let bloccoQuadrati = document.getElementById('blocco-quadrati');
     bloccoQuadrati.innerHTML='';
-    if(livello == "easy"){
+    if(livello == "1"){
         for(let i=0; i<100; i++){
             bloccoQuadrati.innerHTML += '<div class="box"></div>';
     }
-    }else if (livello == "medium"){
+    }else if (livello == "2"){
         for(let k=0; k<81; k++){
             bloccoQuadrati.innerHTML += '<div class="box2"></div>';
     }
-    }else if(livello == "hard"){
+    }else if(livello == "3"){
         for(let j=0; j<49; j++){
             bloccoQuadrati.innerHTML += '<div class="box3"></div>';
         }
